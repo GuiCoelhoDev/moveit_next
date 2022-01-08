051979e6e2a4357583d2c84bf87b70fc115ea401
GuiCoelhoDev <guiminervino@gmail.com>
2022-01-08 20:36:50 -0300

    feat: adiciona componente de botão do countdown
    - permite clicar no botão e o contador diminuir

--- src/components/Countdown/index.tsx
+++ src/components/Countdown/index.tsx
@@ -1,16 +1,47 @@
 import styles from "./styles.module.scss";
+import { useState, useEffect } from "react";
 export function Countdown() {
+  const [time, setTime] = useState(25 * 60); // 25 minutos em seg
+  const [active, setActive] = useState(false);
+
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
+  const [minuteLeft, minuteRight] = String(minutes).padStart(2, "0").split("");
+  const [secondLeft, secondRight] = String(seconds).padStart(2, "0").split("");
+
+  function startCountdown() {
+    setActive(true);
+  }
+
+  useEffect(() => {
+    if (active && time > 0) {
+      setTimeout(() => {
+        setTime(time - 1);
+      }, 1000);
+    }
+  }, [active, time]);
+
   return (
-    <div className={styles.container}>
-      <div>
-        <span>2</span>
-        <span>5</span>
+    <div>
+      <div className={styles.container}>
+        <div>
+          <span>{minuteLeft}</span>
+          <span>{minuteRight}</span>
+        </div>
+        <span>:</span>
+        <div>
+          <span>{secondLeft}</span>
+          <span>{secondRight}</span>
+        </div>
       </div>
-      <span>:</span>
-      <div>
-        <span>0</span>
-        <span>0</span>
-      </div>
+      <button
+        onClick={startCountdown}
+        type="button"
+        className={styles.countdownButton}
+      >
+        Iniciar um Ciclo
+      </button>
     </div>
   );
 }
