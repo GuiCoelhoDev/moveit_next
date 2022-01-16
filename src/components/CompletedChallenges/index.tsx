3231f7a3a34d57b6f1950862d5b557b3cd775ad6
GuiCoelhoDev <guiminervino@gmail.com>
2022-01-16 01:54:40 -0300

    feat: torna dinâmico as challenges completadas

--- src/components/CompletedChallenges/index.tsx
+++ src/components/CompletedChallenges/index.tsx
@@ -1,10 +1,13 @@
+import { useContext } from "react";
+import { ChallengesContext } from "../../contexts/ChallengesContexts";
 import styles from "./styles.module.scss";
 
 export function CompletedChallenges() {
+  const { challengesCompleted } = useContext(ChallengesContext);
   return (
     <div className={styles.container}>
       <span>Desafios completos</span>
-      <span>5</span>
+      <span>{challengesCompleted}</span>
     </div>
   );
 }
